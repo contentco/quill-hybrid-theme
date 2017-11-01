a3f59b44c7c7f2ee9dee62cb358c6d66cece582e
Ye Min <yehtut.it@gmail.com>
2017-11-01 18:09:31 +0800

    move toolbar to original

--- src/theme-contentco.js
+++ src/theme-contentco.js
@@ -9,8 +9,9 @@ let Picker = Quill.import('ui/picker');
 let IconPicker = Quill.import('ui/icon-picker');
 let icons = Quill.import('ui/icons');
 let Delta = Quill.import('delta');
+let Inline = Quill.import('blots/inline');
 
-var Toolbar = Quill.import('modules/toolbar');
+let Toolbar = Quill.import('modules/toolbar');
 
 const ALIGNS = [ false, 'center', 'right', 'justify' ];
 
@@ -34,7 +35,7 @@ const TOOLBAR_CONFIG = [
     ['clean'],['link']
 ];
 
-class Contentco extends BaseTheme {
+class HybridTheme extends BaseTheme {
   constructor(quill, options) {
     if (options.modules.toolbar != null && options.modules.toolbar.container == null) {
       options.modules.toolbar.container = TOOLBAR_CONFIG;
@@ -45,19 +46,67 @@ class Contentco extends BaseTheme {
   }
 
   extendToolbar(toolbar) {
-    toolbar.container.classList.add('ql-bubble');
+    toolbar.container.classList.add('ql-snow');
     this.buildButtons([].slice.call(toolbar.container.querySelectorAll('button')), icons);
     this.buildPickers([].slice.call(toolbar.container.querySelectorAll('select')), icons);
     this.tooltip = new SnowTooltip(this.quill, this.options.bounds);
-    //this.tooltip.root.appendChild(toolbar.container);
-    this.actionBtn = new ContentTooltip(this.quill, this.options.bounds);
-    this.actionBtn.root.appendChild(toolbar.container);
 
     if (toolbar.container.querySelector('.ql-link')) {
       this.quill.keyboard.addBinding({ key: 'K', shortKey: true }, function(range, context) {
         toolbar.handlers['link'].call(toolbar, !context.format.link);
       });
     }
+    toolbar.addHandler('link', this.linkEventHanlder);
+    toolbar.addHandler('video', this.videoEventHanlder);
+  }
+
+  linkEventHanlder(){
+    this.quill.theme.tooltip.edit('link');
+    if (this.quill.theme.tooltip.root.offsetLeft < 0) {
+      this.quill.theme.tooltip.root.style.left = '0px';
+      this.quill.theme.tooltip.textbox.placeholder = '';
+    }
+  }
+
+  videoEventHanlder(){
+    this.quill.theme.tooltip.edit('video');
+    if (this.quill.theme.tooltip.root.offsetLeft < 0) {
+      this.quill.theme.tooltip.root.style.left = '0px';
+    }
+  }
+
+  buildPickers(selects, icons) {
+    this.pickers = selects.map((select) => {
+      if (select.classList.contains('ql-align')) {
+        if (select.querySelector('option') == null) {
+          fillSelect(select, ALIGNS);
+        }
+        return new IconPicker(select, icons.align);
+      } else if (select.classList.contains('ql-background') || select.classList.contains('ql-color')) {
+        let format = select.classList.contains('ql-background') ? 'background' : 'color';
+        if (select.querySelector('option') == null) {
+          fillSelect(select, COLORS, format === 'background' ? '#ffffff' : '#000000');
+        }
+        return new ColorPicker(select, icons[format]);
+      } else {
+        if (select.querySelector('option') == null) {
+          if (select.classList.contains('ql-font')) {
+            fillSelect(select, FONTS);
+          } else if (select.classList.contains('ql-header')) {
+            fillSelect(select, HEADERS);
+          } else if (select.classList.contains('ql-size')) {
+            fillSelect(select, SIZES);
+          }
+        }
+        return new Picker(select);
+      }
+    });
+    let update = () => {
+      this.pickers.forEach(function(picker) {
+        picker.update();
+      });
+    };
+    this.quill.on(Emitter.events.EDITOR_CHANGE, update);
   }
 }
 
@@ -88,7 +137,10 @@ class ContentTooltip {
         let lines = this.quill.getLines(range.index, range.length);
         if (lines.length === 1) {
           //this.position(this.quill.getBounds(range));
-          this.position({bottom:this.quill.getBounds(range).bottom,height:this.quill.getBounds(range).height,left:this.quill.container.offsetWidth - this.quill.getBounds(range).width/2,right: 0,top:this.quill.getBounds(range).top,width:this.quill.getBounds(range).width});
+          this.position({bottom:this.quill.getBounds(range).bottom,
+            height:this.quill.getBounds(range).height,
+            left:this.quill.container.offsetWidth - this.quill.getBounds(range).width/2,
+            right: this.root.offsetWidth/2,top:this.quill.getBounds(range).top - this.root.offsetHeight/2,width:this.quill.getBounds(range).width});
         }
         else {
         //   let lastLine = lines[lines.length - 1];
@@ -136,8 +188,9 @@ class ContentTooltip {
 
   position(reference) {
     let left = reference.left;
+    let right = reference.right;
     let top = reference.top;
-    this.root.style.left = left + 'px';
+    this.root.style.right = '-' + right + 'px';
     this.root.style.top = top + 'px';
   }
 }
@@ -202,63 +255,30 @@ SnowTooltip.TEMPLATE = [
   '<a class="ql-remove"></a>'
 ].join('');
 
-Quill.register('themes/hybrid', Contentco);
+Quill.register('themes/hybrid', HybridTheme);
 
 
-class InlineToolbar extends Toolbar {
+class BubbleActions extends Toolbar {
   constructor(quill, options) {
     super(quill, options);
     this.container.classList.add('ql-toolbar-hybrid');
-    this.container.classList.add('ql-snow');
     this.buildButtons([].slice.call(this.container.querySelectorAll('button')), icons);
-    this.buildPickers([].slice.call(this.container.querySelectorAll('select')), icons);
-    this.addHandler('link', this.linkEventHanlder);
-    this.addHandler('video', this.videoEventHanlder);
-    this.addHandler('image', this.imageEventHanlder);
+    this.actionBtn = new ContentTooltip(this.quill, this.options.bounds);
+    this.actionBtn.root.appendChild(this.container);
+
+    var commentBtns = document.getElementsByClassName('ql-comment');
+    if (commentBtns) {
+       [].slice.call( commentBtns ).forEach(function ( commentBtn ) {
+           commentBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16"><g fill="none" fill-rule="evenodd"><path fill="#444" fill-rule="nonzero" d="M9.92 11H13c1.66 0 3-1.36 3-3V5c0-1.66-1.34-3-3-3H5C3.34 2 2 3.34 2 5v3c0 1.64 1.34 3 3 3h1.44l.63 1.88 2.85-1.9zM5 0h8c2.76 0 5 2.24 5 5v3c0 2.75-2.24 5-5 5h-2.47L7.1 15.26c-.47.3-1.1.2-1.4-.27-.05-.1-.08-.18-.1-.26L5 13c-2.76 0-5-2.25-5-5V5c0-2.76 2.24-5 5-5z"/><path stroke="#444" stroke-width="2" d="M5.37 5H13M5.37 8H10" stroke-linecap="round" stroke-linejoin="round"/></g></svg>';
+       });
+    };
+    this.addHandler('comment', this.commentEventHanlder);
+
   }
 
-  linkEventHanlder(){
-    this.quill.theme.tooltip.edit('link');
-    if (this.quill.theme.tooltip.root.offsetLeft < 0) {
-      this.quill.theme.tooltip.root.style.left = '0px';
-      this.quill.theme.tooltip.textbox.placeholder = '';
-    }
-  }
-
-  videoEventHanlder(){
-    this.quill.theme.tooltip.edit('video');
-    if (this.quill.theme.tooltip.root.offsetLeft < 0) {
-      this.quill.theme.tooltip.root.style.left = '0px';
-    }
-  }
-
-  imageEventHanlder(){
-
-      var fileInput = this.container.querySelector('input.ql-image[type=file]');
-      if (fileInput == null) {
-        fileInput = document.createElement('input');
-        fileInput.setAttribute('type', 'file');
-        fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon');
-        fileInput.classList.add('ql-image');
-        fileInput.addEventListener('change', () => {
-          if (fileInput.files != null && fileInput.files[0] != null) {
-            let reader = new FileReader();
-            reader.onload = (e) => {
-              let range = this.quill.getSelection(true);
-              this.quill.updateContents(new Delta()
-                .retain(range.index)
-                .delete(range.length)
-                .insert({ image: e.target.result })
-              , Emitter.sources.USER);
-              this.quill.setSelection(range.index + 1, Emitter.sources.SILENT);
-              fileInput.value = "";
-            }
-            reader.readAsDataURL(fileInput.files[0]);
-          }
-        });
-        this.container.appendChild(fileInput);
-      }
-    fileInput.click();
+  commentEventHanlder() {
+    let quill = this.quill;
+    checkDialogExist(quill);
   }
 
   buildButtons(buttons, icons) {
@@ -281,52 +301,133 @@ class InlineToolbar extends Toolbar {
       });
     });
   }
-
-  buildPickers(selects, icons) {
-    this.pickers = selects.map((select) => {
-      if (select.classList.contains('ql-align')) {
-        if (select.querySelector('option') == null) {
-          fillSelect(select, ALIGNS);
-        }
-        return new IconPicker(select, icons.align);
-      } else if (select.classList.contains('ql-background') || select.classList.contains('ql-color')) {
-        let format = select.classList.contains('ql-background') ? 'background' : 'color';
-        if (select.querySelector('option') == null) {
-          fillSelect(select, COLORS, format === 'background' ? '#ffffff' : '#000000');
-        }
-        return new ColorPicker(select, icons[format]);
-      } else {
-        if (select.querySelector('option') == null) {
-          if (select.classList.contains('ql-font')) {
-            fillSelect(select, FONTS);
-          } else if (select.classList.contains('ql-header')) {
-            fillSelect(select, HEADERS);
-          } else if (select.classList.contains('ql-size')) {
-            fillSelect(select, SIZES);
-          }
-        }
-        return new Picker(select);
-      }
-    });
-    let update = () => {
-      this.pickers.forEach(function(picker) {
-        picker.update();
-      });
-    };
-    this.quill.on(Emitter.events.EDITOR_CHANGE, update);
-  }
 }
 
-function extractVideoUrl(url) {
-  let match = url.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtube\.com\/watch.*v=([a-zA-Z0-9_-]+)/) ||
-              url.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtu\.be\/([a-zA-Z0-9_-]+)/);
-  if (match) {
-    return (match[1] || 'https') + '://www.youtube.com/embed/' + match[2] + '?showinfo=0';
-  }
-  if (match = url.match(/^(?:(https?):\/\/)?(?:www\.)?vimeo\.com\/(\d+)/)) {  // eslint-disable-line no-cond-assign
-    return (match[1] || 'https') + '://player.vimeo.com/video/' + match[2] + '/';
-  }
-  return url;
+Quill.register('modules/bubble_actions', BubbleActions);
+
+class CommentBlot extends Inline {
+    static create(commentText) {
+        const node = super.create();
+        node.dataset.comment = commentText.comment;
+        if (commentText.id) {
+            node.dataset.id = commentText.id;
+        }
+        if (commentText.resolved) {
+            node.dataset.resolved = commentText.resolved;
+        }
+
+        return node;
+    }
+    static formats(node) {
+        return node.dataset;
+    }
+    format(name, value) {
+        super.format(name, value);
+    }
+}
+
+CommentBlot.blotName = "comment";
+CommentBlot.tagName = "SPAN";
+CommentBlot.className = "annotation";
+
+Quill.register({
+    'formats/comment': CommentBlot
+});
+
+
+class GrammlyBlot extends Inline {
+    static create(commentText) {
+        const node = super.create();
+
+        return node;
+    }
+    static formats(node) {
+        return node.dataset;
+    }
+    format(name, value) {
+        super.format(name, value);
+    }
+}
+
+GrammlyBlot.blotName = "grammer";
+GrammlyBlot.tagName = "SPAN";
+GrammlyBlot.className = "gr_";
+
+Quill.register({
+    'formats/grammer': GrammlyBlot
+});
+
+
+function checkDialogExist(quill){
+    let commentToolTip = document.getElementById("inline-comment");
+    let commentMask = document.getElementById("inline-comment-mask");
+    if (commentToolTip) {
+        commentToolTip.remove();
+        commentMask.remove();
+    }
+    else{
+        createCommentDialog(quill);
+    }
+}
+
+function createCommentDialog(quill) {
+    let range = quill.getSelection();
+    let text = quill.getText(range.index, range.length);
+    if (text.length < 1) {
+        return;
+    }
+    const atSignBounds = quill.getBounds(range.index);
+    let containerMask = document.createElement('div');
+    containerMask.id="inline-comment-mask";
+    containerMask.style.width   = "100%";
+    containerMask.style.height   = "100%";
+    containerMask.style.top   = "0px";
+    containerMask.style.position   = "fixed";
+    containerMask.style.display   = "block";
+
+    let container  = document.createElement('div');
+    container.id =  'inline-comment';
+    container.classList.add('inline-comment');
+    quill.container.appendChild(container);
+    quill.container.appendChild(containerMask);
+    container.style.position   = "absolute";
+    container.innerHTML = '<textarea class="commentText" placeholder="Type your comment"></textarea><div class="inline-comment-bottom"><span class="inline-cancel">Cancel</span> <span class="inline-send">Send</span> </div>';
+
+
+    container.style.left = (atSignBounds.left - 250)+ "px";
+
+    if (atSignBounds.left + 250 < quill.container.clientWidth) {
+        container.style.left = (atSignBounds.left)+ "px";
+    }
+
+    container.style.top = 10 + atSignBounds.top + atSignBounds.height + "px";
+    container.style.zIndex = 80;
+    document.querySelector('.commentText').focus();
+
+    let inlineCancel = document.querySelector('.inline-cancel');
+    let commentToolTip = document.querySelector('.inline-comment');
+
+    inlineCancel.addEventListener('click',function(){
+        commentToolTip.style.display    = "none";
+        containerMask.style.display     = "none";
+    });
+
+    let inlineSend = document.querySelector('.inline-send');
+
+    inlineSend.addEventListener('click',function(){
+        const commentObj = {};
+        let commentText = document.querySelector('.commentText').value;
+        commentObj.comment = commentText;
+        if (typeof(commentId) !== 'undefined') {
+            commentObj.id= commentId;
+        }
+        if (typeof(commentStatus) !== 'undefined') {
+            commentObj.resolved= commentStatus;
+        }
+        commentToolTip.remove();
+        containerMask.remove();
+        quill.format('comment', commentObj);
+    });
 }
 
 function fillSelect(select, values, defaultValue = false) {
@@ -340,58 +441,3 @@ function fillSelect(select, values, defaultValue = false) {
     select.appendChild(option);
   });
 }
-
-function addControls(container, groups) {
-  if (!Array.isArray(groups[0])) {
-    groups = [groups];
-  }
-  groups.forEach(function(controls) {
-    let group = document.createElement('span');
-    group.classList.add('ql-formats');
-    controls.forEach(function(control) {
-      if (typeof control === 'string') {
-        addButton(group, control);
-      } else {
-        let format = Object.keys(control)[0];
-        let value = control[format];
-        if (Array.isArray(value)) {
-          addSelect(group, format, value);
-        } else {
-          addButton(group, format, value);
-        }
-      }
-    });
-    container.appendChild(group);
-  });
-}
-
-function addSelect(container, format, values) {
-  let input = document.createElement('select');
-  input.classList.add('ql-' + format);
-  values.forEach(function(value) {
-    let option = document.createElement('option');
-    if (value !== false) {
-      option.setAttribute('value', value);
-    } else {
-      option.setAttribute('selected', 'selected');
-    }
-    input.appendChild(option);
-  });
-  container.appendChild(input);
-}
-
-InlineToolbar.DEFAULTS = {};
-
-
-function addButton(container, format, value) {
-  let input = document.createElement('button');
-  input.setAttribute('type', 'button');
-  input.classList.add('ql-' + format);
-  if (value != null) {
-    input.value = value;
-  }
-  container.appendChild(input);
-}
-
-
-Quill.register('modules/inline_toolbar', InlineToolbar);
