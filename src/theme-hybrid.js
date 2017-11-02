83455fd2aba573f2a49b32b9993adc66cfe01331
Ye Min <yehtut.it@gmail.com>
2017-11-02 12:11:33 +0800

    add eslint

--- src/theme-hybrid.js
+++ src/theme-hybrid.js
@@ -15,13 +15,7 @@ let Toolbar = Quill.import('modules/toolbar');
 
 const ALIGNS = [ false, 'center', 'right', 'justify' ];
 
-const COLORS = [
-  "#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff",
-  "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff",
-  "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff",
-  "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2",
-  "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466"
-];
+const COLORS = ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff','#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff','#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff','#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2','#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466'];
 
 const FONTS = [ false, 'serif', 'monospace' ];
 
@@ -29,11 +23,7 @@ const HEADERS = [ '1', '2', '3', false ];
 
 const SIZES = [ 'small', false, 'large', 'huge' ];
 
-const TOOLBAR_CONFIG = [
-    ['bold', 'italic', 'underline', 'link'],
-    [{ list: 'ordered' }, { list: 'bullet' }],
-    ['clean'],['link']
-];
+const TOOLBAR_CONFIG = [['bold', 'italic', 'underline', 'link'],[{ list: 'ordered' }, { list: 'bullet' }],['clean'],['link']];
 
 class HybridTheme extends BaseTheme {
   constructor(quill, options) {
@@ -113,7 +103,7 @@ class HybridTheme extends BaseTheme {
 
 
 class ContentTooltip {
-  constructor(quill, boundsContainer) {
+  constructor(quill) {
     this.quill = quill;
     this.boundsContainer = this.quill.container || document.body;
     this.root = quill.addContainer('ql-bubble-actions');
@@ -186,8 +176,6 @@ class ContentTooltip {
   }
 
   position(reference) {
-    let left = reference.left;
-    let right = reference.right;
     let top = reference.top;
     this.root.style.right = '0px';
     this.root.style.top = top + 'px';
@@ -268,12 +256,11 @@ class BubbleActions extends Toolbar {
 
     var commentBtns = document.getElementsByClassName('ql-comment');
     if (commentBtns) {
-       [].slice.call( commentBtns ).forEach(function ( commentBtn ) {
-           commentBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16"><g fill="none" fill-rule="evenodd"><path fill="#444" fill-rule="nonzero" d="M9.92 11H13c1.66 0 3-1.36 3-3V5c0-1.66-1.34-3-3-3H5C3.34 2 2 3.34 2 5v3c0 1.64 1.34 3 3 3h1.44l.63 1.88 2.85-1.9zM5 0h8c2.76 0 5 2.24 5 5v3c0 2.75-2.24 5-5 5h-2.47L7.1 15.26c-.47.3-1.1.2-1.4-.27-.05-.1-.08-.18-.1-.26L5 13c-2.76 0-5-2.25-5-5V5c0-2.76 2.24-5 5-5z"/><path stroke="#444" stroke-width="2" d="M5.37 5H13M5.37 8H10" stroke-linecap="round" stroke-linejoin="round"/></g></svg>';
-       });
-    };
+      [].slice.call( commentBtns ).forEach(function ( commentBtn ) {
+        commentBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16"><g fill="none" fill-rule="evenodd"><path fill="#444" fill-rule="nonzero" d="M9.92 11H13c1.66 0 3-1.36 3-3V5c0-1.66-1.34-3-3-3H5C3.34 2 2 3.34 2 5v3c0 1.64 1.34 3 3 3h1.44l.63 1.88 2.85-1.9zM5 0h8c2.76 0 5 2.24 5 5v3c0 2.75-2.24 5-5 5h-2.47L7.1 15.26c-.47.3-1.1.2-1.4-.27-.05-.1-.08-.18-.1-.26L5 13c-2.76 0-5-2.25-5-5V5c0-2.76 2.24-5 5-5z"/><path stroke="#444" stroke-width="2" d="M5.37 5H13M5.37 8H10" stroke-linecap="round" stroke-linejoin="round"/></g></svg>';
+      });
+    }
     this.addHandler('comment', this.commentEventHanlder);
-
   }
 
   commentEventHanlder() {
@@ -306,128 +293,124 @@ class BubbleActions extends Toolbar {
 Quill.register('modules/bubble_actions', BubbleActions);
 
 class CommentBlot extends Inline {
-    static create(commentText) {
-        const node = super.create();
-        node.dataset.comment = commentText.comment;
-        if (commentText.id) {
-            node.dataset.id = commentText.id;
-        }
-        if (commentText.resolved) {
-            node.dataset.resolved = commentText.resolved;
-        }
-
-        return node;
+  static create(commentText) {
+    const node = super.create();
+    node.dataset.comment = commentText.comment;
+    if (commentText.id) {
+      node.dataset.id = commentText.id;
     }
-    static formats(node) {
-        return node.dataset;
+    if (commentText.resolved) {
+      node.dataset.resolved = commentText.resolved;
     }
-    format(name, value) {
-        super.format(name, value);
-    }
+    return node;
+  }
+  static formats(node) {
+    return node.dataset;
+  }
+  format(name, value) {
+    super.format(name, value);
+  }
 }
 
-CommentBlot.blotName = "comment";
-CommentBlot.tagName = "SPAN";
-CommentBlot.className = "annotation";
+CommentBlot.blotName = 'comment';
+CommentBlot.tagName = 'SPAN';
+CommentBlot.className = 'annotation';
 
 Quill.register({
-    'formats/comment': CommentBlot
+  'formats/comment': CommentBlot
 });
 
 
 class GrammlyBlot extends Inline {
-    static create(commentText) {
-        const node = super.create();
-
-        return node;
-    }
-    static formats(node) {
-        return node.dataset;
-    }
-    format(name, value) {
-        super.format(name, value);
-    }
+  static create() {
+    const node = super.create();
+    return node;
+  }
+  static formats(node) {
+    return node.dataset;
+  }
+  format(name, value) {
+    super.format(name, value);
+  }
 }
 
-GrammlyBlot.blotName = "grammer";
-GrammlyBlot.tagName = "SPAN";
-GrammlyBlot.className = "gr_";
+GrammlyBlot.blotName = 'grammer';
+GrammlyBlot.tagName = 'SPAN';
+GrammlyBlot.className = 'gr_';
 
 Quill.register({
-    'formats/grammer': GrammlyBlot
+  'formats/grammer': GrammlyBlot
 });
 
 
 function checkDialogExist(quill){
-    let commentToolTip = document.getElementById("inline-comment");
-    let commentMask = document.getElementById("inline-comment-mask");
-    if (commentToolTip) {
-        commentToolTip.remove();
-        commentMask.remove();
-    }
-    else{
-        createCommentDialog(quill);
-    }
+  let commentToolTip = document.getElementById('inline-comment');
+  let commentMask = document.getElementById('inline-comment-mask');
+  if (commentToolTip) {
+    commentToolTip.remove();
+    commentMask.remove();
+  }
+  else{
+    createCommentDialog(quill);
+  }
 }
 
 function createCommentDialog(quill) {
-    let range = quill.getSelection();
-    let text = quill.getText(range.index, range.length);
-    if (text.length < 1) {
-        return;
-    }
-    const atSignBounds = quill.getBounds(range.index);
-    let containerMask = document.createElement('div');
-    containerMask.id="inline-comment-mask";
-    containerMask.style.width   = "100%";
-    containerMask.style.height   = "100%";
-    containerMask.style.top   = "0px";
-    containerMask.style.position   = "fixed";
-    containerMask.style.display   = "block";
+  let range = quill.getSelection();
+  let text = quill.getText(range.index, range.length);
+  if (text.length < 1) {
+    return;
+  }
+  const atSignBounds = quill.getBounds(range.index);
+  let containerMask = document.createElement('div');
+  containerMask.id='inline-comment-mask';
+  containerMask.style.width   = '100%';
+  containerMask.style.height   = '100%';
+  containerMask.style.top   = '0px';
+  containerMask.style.position   = 'fixed';
+  containerMask.style.display   = 'block';
 
-    let container  = document.createElement('div');
-    container.id =  'inline-comment';
-    container.classList.add('inline-comment');
-    quill.container.appendChild(container);
-    quill.container.appendChild(containerMask);
-    container.style.position   = "absolute";
-    container.innerHTML = '<textarea class="commentText" placeholder="Type your comment"></textarea><div class="inline-comment-bottom"><span class="inline-cancel">Cancel</span> <span class="inline-send">Send</span> </div>';
+  let container  = document.createElement('div');
+  container.id =  'inline-comment';
+  container.classList.add('inline-comment');
+  quill.container.appendChild(container);
+  quill.container.appendChild(containerMask);
+  container.style.position   = 'absolute';
+  container.innerHTML = '<textarea class="commentText" placeholder="Type your comment"></textarea><div class="inline-comment-bottom"><span class="inline-cancel">Cancel</span> <span class="inline-send">Send</span> </div>';
+  container.style.left = (atSignBounds.left - 250)+ 'px';
 
+  if (atSignBounds.left + 250 < quill.container.clientWidth) {
+    container.style.left = (atSignBounds.left)+ 'px';
+  }
 
-    container.style.left = (atSignBounds.left - 250)+ "px";
+  container.style.top = 10 + atSignBounds.top + atSignBounds.height + 'px';
+  container.style.zIndex = 80;
+  document.querySelector('.commentText').focus();
 
-    if (atSignBounds.left + 250 < quill.container.clientWidth) {
-        container.style.left = (atSignBounds.left)+ "px";
-    }
+  let inlineCancel = document.querySelector('.inline-cancel');
+  let commentToolTip = document.querySelector('.inline-comment');
 
-    container.style.top = 10 + atSignBounds.top + atSignBounds.height + "px";
-    container.style.zIndex = 80;
-    document.querySelector('.commentText').focus();
+  inlineCancel.addEventListener('click',function(){
+    commentToolTip.style.display    = 'none';
+    containerMask.style.display     = 'none';
+  });
 
-    let inlineCancel = document.querySelector('.inline-cancel');
-    let commentToolTip = document.querySelector('.inline-comment');
+  let inlineSend = document.querySelector('.inline-send');
 
-    inlineCancel.addEventListener('click',function(){
-        commentToolTip.style.display    = "none";
-        containerMask.style.display     = "none";
-    });
-
-    let inlineSend = document.querySelector('.inline-send');
-
-    inlineSend.addEventListener('click',function(){
-        const commentObj = {};
-        let commentText = document.querySelector('.commentText').value;
-        commentObj.comment = commentText;
-        if (typeof(commentId) !== 'undefined') {
-            commentObj.id= commentId;
-        }
-        if (typeof(commentStatus) !== 'undefined') {
-            commentObj.resolved= commentStatus;
-        }
-        commentToolTip.remove();
-        containerMask.remove();
-        quill.format('comment', commentObj);
-    });
+  inlineSend.addEventListener('click',function(){
+    const commentObj = {};
+    let commentText = document.querySelector('.commentText').value;
+    commentObj.comment = commentText;
+    // if (typeof(commentId) !== 'undefined') {
+    //   commentObj.id= commentId;
+    // }
+    // if (typeof(commentStatus) !== 'undefined') {
+    //   commentObj.resolved= commentStatus;
+    // }
+    commentToolTip.remove();
+    containerMask.remove();
+    quill.format('comment', commentObj);
+  });
 }
 
 function fillSelect(select, values, defaultValue = false) {
